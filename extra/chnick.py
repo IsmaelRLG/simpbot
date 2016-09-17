b892af57ae5b7b8c7f65de35ce71175f73ebae11
Ismael Lugo (kwargs) <ismaelrlgv@gmail.com>
2016-09-17 16:09:49 -0400

    SimpBot - Update

--- extra/chnick.py
+++ extra/chnick.py
@@ -7,3 +7,5 @@ import simpbot
 
 @simpbot.commands.addCommand('nick !{new_nick}', 'Cambia de nick', 'nick')
 @simpbot.commands.admin
+def chnick(irc, ev, result, target, channel, _):
+    irc.nick(_['new_nick'])
